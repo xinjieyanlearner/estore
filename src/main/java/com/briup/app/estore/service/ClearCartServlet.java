4ad5d7bdfd5b3b97cac9919ff00f1f758479ebb9
jack <jack@qq.com>
2019-11-17 18:49:37 +0800

    add ClearCartServlet.java

---
+++ src/main/java/com/briup/app/estore/service/ClearCartServlet.java
@@ -0,0 +1,29 @@
+package com.briup.app.estore.web.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.briup.app.estore.shoppingcart.ShoppingCart;
+
+@WebServlet("/clear")
+public class ClearCartServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ShoppingCart shoppingCart = (ShoppingCart)request.getSession().getAttribute("shoppingCart");
+		shoppingCart.clear();
+		request.getRequestDispatcher("index.jsp").forward(request, response);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
